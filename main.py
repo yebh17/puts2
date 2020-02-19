4bbff15b23fed71dd61b9cefd8e6549e4d1bcdd0
yebh17 <sunnyrebels0@gmail.com>
2020-02-19 14:01:32 +0100

    add & sub

--- main.py
+++ main.py
@@ -8,11 +8,18 @@ def index():
 
 
 @app.route('/sub', methods = ['POST', 'GET'])
-def addition():
+def substraction():
     value1=request.args.get('A',default = 0, type = int)
     value2=request.args.get('B',default = 0, type = int)
     result=value1-value2
     return '%d \n' % result
+@app.route('/add', methods = ['POST', 'GET'])
+def addition():
+    value1=request.args.get('A',default = 0, type = int)
+    value2=request.args.get('B',default = 0, type = int)
+    result=value1+value2
+    return '%d \n' % result
+
 
 
 if __name__ == "__main__":
--- test_main.py
+++ test_main.py
@@ -12,6 +12,10 @@ class TestCalculator(unittest.TestCase):
     def test_sub(self):
         resp = self.app.get('/sub?A=5&B=1')
         self.assertEqual(b'4 \n', resp.data)
+    # Testing for addition using integers
+    def test_add(self):
+        resp = self.app.get('/add?A=1&B=5')
+        self.assertEqual(b'6 \n', resp.data)
 
 if __name__ == '__main__':
     unittest.main()
