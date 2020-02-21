20923e4777e02b106a94b23e3b4c9078b4d0c5cd
yebh17 <sunnyrebels0@gmail.com>
2020-02-21 16:12:09 +0100

    Satisfy the rational numbers conditions and raise error message if invalid inputs are given and also write test scripts accordingly with various test cases

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+from fractions import Fraction
 
 app = Flask(__name__)
 
@@ -29,11 +30,35 @@ def multiplication():
 
 @app.route('/div', methods = ['POST', 'GET'])
 def division():
-    value1=request.args.get('A',default = 0, type = int)
-    value2=request.args.get('B',default = 0, type = int)
-    result=value1/value2
-    return '%d \n' % result
+    if request.method == 'POST':
+        value1 = request.values.get('A', default=0, type=str)
+    else:
+        value1 = request.args.get('A', default=0, type=str)
 
+    if request.method == 'GET':
+        value2 = request.args.get('B', default=0, type=str)
+    else:
+        value2 = request.values.get('B', default=0, type=str)
+    try:
+        value1 = Fraction(value1)
+    except ZeroDivisionError:
+        return "Error: undefined value!, denominator should not be a zero! \n"
+    except ValueError:
+        return "Error: The A's value should be a numerical! \n"
+    try:
+        value2 = Fraction(value2)
+    except ValueError:
+        return "Error: The A's value should be a numerical! \n"
+    except ZeroDivisionError:
+        return "Error: undefined value!, denominator should not be a zero! \n"
+    try: 
+        result = ((value1)/(value2))
+    except ZeroDivisionError:
+        return "Error: undefined value!, denominator should not be a zero! \n"
+    else:
+        if float(result).is_integer():
+            result = int(result)
+            return("Value by dividing A & B is: " '%d \n' % result)
+        return("Roundup value upto three digits by dividing A & B values is: " '%.3f \n' % result)
 if __name__ == "__main__":
     app.run()
-
--- test_main.py
+++ test_main.py
@@ -20,9 +20,24 @@ class TestCalculator(unittest.TestCase):
     def test_mul(self):
         resp = self.app.get('/mul?A=1&B=5')
         self.assertEqual(b'5 \n', resp.data)
-    # Testing for division using integers
+    # Testing for division
     def test_div(self):
-        resp = self.app.get('/div?A=5&B=1')
-        self.assertEqual(b'5 \n', resp.data)
+
+        # Testing for division using integers.
+        resp = self.app.get('/div?A=24&B=12')
+        self.assertEqual(b'Value by dividing A & B is: 2 \n', resp.data)
+
+        # Testing for division for rational values.
+        resp = self.app.get('/div?A=1/3&B=2/3')
+        self.assertEqual(b'Roundup value upto three digits by dividing A & B values is: 0.500 \n', resp.data)
+
+        # Testing for division for integer values.
+        resp = self.app.get('/div?A=11&B=7')
+        self.assertEqual(b'Roundup value upto three digits by dividing A & B values is: 1.571 \n', resp.data)
+
+        # Testing for division for both float values.
+        resp = self.app.get('/div?A=0.2&B=0.3')
+        self.assertEqual(b'Roundup value upto three digits by dividing A & B values is: 0.667 \n', resp.data)
+
 if __name__ == '__main__':
     unittest.main()
